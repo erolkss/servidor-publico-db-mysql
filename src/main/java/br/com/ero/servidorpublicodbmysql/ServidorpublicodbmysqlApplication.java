49294842027a168e4d362205bae9e348d48e5050
Lucas Lima <lucaserolima@gmail.com>
2024-02-26 21:32:03 -0400

    First Commit

---
+++ src/main/java/br/com/ero/servidorpublicodbmysql/ServidorpublicodbmysqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.servidorpublicodbmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorpublicodbmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorpublicodbmysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ero/servidorpublicodbmysql/ServidorpublicodbmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.servidorpublicodbmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorpublicodbmysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
