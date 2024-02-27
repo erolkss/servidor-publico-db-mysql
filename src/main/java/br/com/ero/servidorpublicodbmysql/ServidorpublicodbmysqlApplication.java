68e0311d98efe1d511a58b569fcbbbf2c3952885
Lucas Lima <lucaserolima@gmail.com>
2024-02-26 22:50:33 -0400

    set Property from JOptionPane work

--- src/main/java/br/com/ero/servidorpublicodbmysql/ServidorpublicodbmysqlApplication.java
+++ src/main/java/br/com/ero/servidorpublicodbmysql/ServidorpublicodbmysqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServidorpublicodbmysqlApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "false");
 		SpringApplication.run(ServidorpublicodbmysqlApplication.class, args);
 	}
 
